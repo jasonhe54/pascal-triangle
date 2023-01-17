b19684f557e45046d4e5b05138b1d530f42e4ce6
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2023-01-16 21:02:35 -0500

    Update: New design layout and first Research Question.

--- listeners.js
+++ listeners.js
@@ -8,3 +8,14 @@ document.getElementById("NumberOfRowsForm").addEventListener("submit", function(
     // create the triangle
     createTriangles(numberOfRows);
 });
+
+// document.getElementById("TriangleScaleForm").addEventListener("submit", function(e) {
+//     // prevent the form from submitting
+//     e.preventDefault();
+//     // get the value of the input
+//     let scaleValue = document.getElementById("TriangleScale_SettingInput").value;
+//     // clear the input field
+//     document.getElementById("NumberOfRows_SettingInput").value = "";
+//     document.getElementById("pTriangle").style.transform = "scale("+scaleValue+")";
+//     document.getelementById("TriangleScale_Current").innerText = scaleValue;
+// });
---
+++ scroll.js
@@ -0,0 +1,4 @@
+function scrollToResearchSection() {
+    let researchSection = document.getElementById("ResearchQuestionSection");
+    researchSection.scrollIntoView({behavior: "smooth"});
+};
