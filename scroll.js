e5938a871d1dbe0fa053eb8d8bbbeef02bb89fc8
jasonhe54 <68795646+jasonhe54@users.noreply.github.com>
2023-01-20 20:44:38 -0500

    Adds a Easy to Use Proof of Concept section to Research Q3

--- scroll.js
+++ scroll.js
@@ -2,3 +2,8 @@ function scrollToResearchSection() {
     let researchSection = document.getElementById("ResearchQuestionSection");
     researchSection.scrollIntoView({behavior: "smooth"});
 };
+
+function scrollToConfigurationPanel() {
+    let configurationPanel = document.getElementById("ConfigurationPanel");
+    configurationPanel.scrollIntoView({behavior: "smooth"});
+}
